934733c3ced5d7bce1790717d1c8db5c8a5e9bf0
Kirito <2033982912@qq.com>
2026-03-28 16:06:16 +0800

    finished

--- lab03/ex5_factorial.s
+++ lab03/ex5_factorial.s
@@ -29,13 +29,13 @@ factorial:
     # YOUR CODE HERE
     addi sp sp -8
     sw a0 0(sp)
-    sw t0 4(sp)
+    sw ra 4(sp)
     bne a0 x0 RecursiveStep
 BaseCase:
     li a0 1
     j Epilogue
 RecursiveStep:
-    addi a0 -1
+    addi a0 a0 -1
     jal ra factorial
     lw t0 0(sp)
     mul a0 t0 a0
