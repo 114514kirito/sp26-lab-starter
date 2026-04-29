a761b8d5ff6999e5508dc3396be25254e2722d34
231220000-Zhang San <2033982912.com>
2026-04-29 21:09:59 +0800

    new

--- lab07/ex1.c
+++ lab07/ex1.c
@@ -53,6 +53,26 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
 
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* YOUR CODE GOES HERE */
+        __m128i sum_vec = _mm_setzero_si128(); // This is a vector initialized to all 0s, you can use this to accumulate your sums in SIMD form
+        for(unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+            __m128i v = _mm_loadu_si128((__m128i*)(vals + i)); // Load 4 integers from vals into a vector
+            __m128i mask = _mm_cmpgt_epi32(v, _127);
+             v= _mm_and_si128 (v, mask); // Zero out elements of v that are less than 128, since we only want to sum values >= 128
+            sum_vec = _mm_add_epi32(sum_vec, v); // Add the values in v to sum_vec, but only where the mask is true (i.e. where vals[i] >= 128)
+             // Compare each element to 127, creating a mask
+
+            // Now you need to horizontally add the elements in filtered and add to result
+            // You can use _mm_hadd_epi32 for this, but be careful since it only adds adjacent pairs
+        }
+         int temp[4];
+         _mm_storeu_si128((__m128i*)temp, sum_vec);
+        result += temp[0] + temp[1] + temp[2] + temp[3];
+        for(unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+                result += vals[i];
+            }
+        }
+        // Handle the tail case for when NUM_ELEMS isn't a multiple of 4
 
         /* Hint: you'll need a tail case. */
     }
@@ -70,7 +90,44 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
     /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
 
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+        __m128i sum_vec_0 = _mm_setzero_si128();
+        __m128i sun_vec_1 = _mm_setzero_si128();
+        __m128i sum_vec_2 = _mm_setzero_si128();
+        __m128i sum_vec_3 = _mm_setzero_si128();
         /* YOUR CODE GOES HERE */
+        for(unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16) {
+            __m128i v1 = _mm_loadu_si128((__m128i*)(vals + i)); // Load 4 integers from vals into a vector
+            __m128i v2 = _mm_loadu_si128((__m128i*)(vals + i + 4));
+            __m128i v3 = _mm_loadu_si128((__m128i*)(vals + i + 8));
+            __m128i v4 = _mm_loadu_si128((__m128i*)(vals + i + 12));
+            __m128i mask1 = _mm_cmpgt_epi32(v1, _127);
+            __m128i mask2 = _mm_cmpgt_epi32(v2, _127);
+            __m128i mask3 = _mm_cmpgt_epi32(v3, _127);
+            __m128i mask4 = _mm_cmpgt_epi32(v4, _127);
+             v1= _mm_and_si128 (mask1, v1);
+             v2= _mm_and_si128 (mask2, v2);
+             v3= _mm_and_si128 (mask3, v3);
+             v4= _mm_and_si128 (mask4, v4);
+            sum_vec_0 = _mm_add_epi32(sum_vec_0, v1);
+            sun_vec_1 = _mm_add_epi32(sun_vec_1, v2);
+            sum_vec_2 = _mm_add_epi32(sum_vec_2, v3);
+            sum_vec_3 = _mm_add_epi32(sum_vec_3, v4);
+        }
+         int temp[4];
+        _mm_storeu_si128((__m128i*)temp, sum_vec_0);
+        result += temp[0] + temp[1] + temp[2] + temp[3];
+        _mm_storeu_si128((__m128i*)temp, sun_vec_1);
+        result += temp[0] + temp[1] + temp[2] + temp[3];
+        _mm_storeu_si128((__m128i*)temp, sum_vec_2);
+        result += temp[0] + temp[1] + temp[2] + temp[3];
+        _mm_storeu_si128((__m128i*)temp, sum_vec_3);
+        result += temp[0] + temp[1] + temp[2] + temp[3];
+
+         for(int i=NUM_ELEMS /16*16;i<NUM_ELEMS;i++){
+            if(vals[i]>=128){
+                result+=vals[i];
+            }
+         }
         /* Copy your sum_simd() implementation here, and unroll it */
 
         /* Hint: you'll need 1 or maybe 2 tail cases here. */
--- lab07/ex2.c
+++ lab07/ex2.c
@@ -1,5 +1,7 @@
 #include "ex2.h"
-
+#include <omp.h>
+#include <immintrin.h>
+#include <stdio.h>
 double dotp_naive(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     for (int i = 0; i < arr_size; i++)
@@ -11,6 +13,14 @@ double dotp_naive(double* x, double* y, int arr_size) {
 double dotp_critical(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
+    #pragma omp parallel for
+    for (int i = 0; i < arr_size; i++) {
+        double product = x[i] * y[i];
+        #pragma omp critical
+        {
+            global_sum += product;
+        }
+    }
     // Use the critical keyword here!
     return global_sum;
 }
@@ -18,7 +28,11 @@ double dotp_critical(double* x, double* y, int arr_size) {
 // Reduction Keyword
 double dotp_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
-    // TODO: Implement this function
+    // TODO: Implement this functiono
+    #pragma omp parallel for reduction(+:global_sum)
+    for (int i = 0; i < arr_size; i++) {
+        global_sum += x[i] * y[i];
+    }
     // Use the reduction keyword here!
     return global_sum;
 }
@@ -30,10 +44,36 @@ double dotp_manual_reduction(double* x, double* y, int arr_size) {
     // Do NOT use the `reduction` directive here!
     #pragma omp parallel
     {
-        double local_sum = 0.0;
+        double local_sum =0;
+
         // TODO: Parallel Section
+        __m256d local_sum_vec = _mm256_setzero_pd(); // Initialize a vector to hold the local sum for each thread
+
+        #pragma omp for
+        for (int i = 0; i < arr_size/4 *4; i+=4) {
+            __m256d x_vec = _mm256_loadu_pd(&x[i]); // Load 4 doubles from x into a vector
+            __m256d y_vec = _mm256_loadu_pd(&y[i]); // Load 4 doubles from y into a vector
+            __m256d product_vec = _mm256_mul_pd(x_vec, y_vec); // Element-wise multiply the two vectors
+            local_sum_vec = _mm256_add_pd(local_sum_vec, product_vec); // Accumulate the products into the local sum vector
+        }
+        double temp[4];
+        _mm256_storeu_pd((__m256d*)temp, local_sum_vec);
+         // Store the local sum vector
+        local_sum += temp[0] + temp[1] + temp[2] + temp[3];
+        // Horizontally add the elements in the local sum vector to get the total local sum for this thread
+
+        for (int i = arr_size/4 *4; i < arr_size; i++) {
+            local_sum += x[i] * y[i];
+        }
+
+
+
 
         // TODO: Critical Section
+        #pragma omp critical
+        {
+            global_sum += local_sum;
+        }
     }
     return global_sum;
 }
